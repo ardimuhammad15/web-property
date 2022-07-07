43e3ab0b210c1db46856411d61f17429147566c4
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2022-07-08 03:29:34 +0700

    init commit and database

---
+++ database/migrations/2022_07_07_192652_create_checkouts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('house_id');
+            $table->integer('rent_id');
+            $table->integer('discounts_id');
+            $table->string('card_number');
+            $table->date('expired');
+            $table->string('cvc');
+            $table->boolean('is_paid');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
