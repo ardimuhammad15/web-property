7eb6626053020f3226ed7954e28aad8f63c9bc66
harisad <harism@upnvj.ac.id>
2022-07-08 16:21:58 +0700

    fix banget nih

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::get('/unit', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
